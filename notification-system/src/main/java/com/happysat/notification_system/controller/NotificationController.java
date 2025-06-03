a11ff5102beb182ff0f99d0980e7779a433409ed
anonymous-lad2 <anonymouslad540@gmail.com>
2025-06-03 14:38:07 +0530

    server side

---
+++ notification-system/src/main/java/com/happysat/notification_system/controller/NotificationController.java
@@ -0,0 +1,18 @@
+package com.happysat.notification_system.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class NotificationController {
+
+    // server application
+    // /app/sendMessage
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/notification")
+    public String sendMessage(String message){
+        System.out.println("message : " +message);
+        return message;
+    }
+}
