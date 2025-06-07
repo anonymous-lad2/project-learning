ae3c446bfe5d8804c96cf25e86c0676f2dc38189
anonymous-lad2 <anonymouslad540@gmail.com>
2025-06-07 19:07:51 +0530

    path variables and query parameters

--- books/src/main/java/com/happysat/books/controller/BookController.java
+++ books/src/main/java/com/happysat/books/controller/BookController.java
@@ -1,8 +1,7 @@
 package com.happysat.books.controller;
 
 import com.happysat.books.model.Book;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,10 +17,10 @@ public class BookController {
 
     private void initializeBooks(){
         books.addAll(List.of(
-                new Book("Title one", "Author one", "physics"),
+                new Book("Title one", "Author one", "science"),
                 new Book("Title two", "Author two", "maths"),
                 new Book("Title three", "Author three", "english"),
-                new Book("Title four", "Author four", "chemistry"),
+                new Book("Title four", "Author four", "science"),
                 new Book("Title five", "Author five", "java"),
                 new Book("Title six", "Author six", "social studies")
         ));
@@ -33,7 +32,39 @@ public class BookController {
     }
 
     @GetMapping("/api/books")
-    public List<Book> getAllBooks(){
-        return books;
+    public List<Book> getBookByCategory(@RequestParam(required = false) String category){
+
+        if(category == null){
+            return books;
+        }
+//        List<Book> filteredBooks = new ArrayList<>();
+//        for(Book book : books){
+//            if(book.getCategory().equalsIgnoreCase(category)){
+//                filteredBooks.add(book);
+//            }
+//        }
+//        return filteredBooks;
+
+        return books.stream()
+                .filter(book -> book.getCategory().equalsIgnoreCase(category))
+                .toList();
     }
+
+    @GetMapping("/api/books/{title}")
+    public Book getBookByTitle(@PathVariable String title){
+
+//        for(Book book : books){
+//            if(book.getTitle().equalsIgnoreCase(title)){
+//                return book;
+//            }
+//        }
+//        return null;
+
+        return books.stream()
+                .filter(book -> book.getTitle().equalsIgnoreCase(title))
+                .findFirst()
+                .orElse(null);
+    }
+
+
 }
